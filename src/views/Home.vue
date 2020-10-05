7f823f373b93537ab071e2bfca8e20858c85dc89
wenom64 <vit.pecena@quanti.cz>
2020-10-05 11:55:33 +0200

    Add buefy CSS framework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-	<div>Hello</div>
+	<div>
+		<div>Hello</div>
+
+		<b-button @click="clickMe">Click Me</b-button>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "Home",
+
+	methods: {
+		clickMe() {
+			this.$buefy.notification.open("Clicked!!");
+		},
+	},
 };
 </script>
