d94292bb94e85212a00ba676085ea57da696d48b
wenom64 <vit.pecena@quanti.cz>
2020-10-06 14:13:52 +0200

    Added api prefix to base path

--- src/const.js
+++ src/const.js
@@ -1,5 +1,5 @@
 const API_HOST = process.env.VUE_APP_API_HOST || "http://localhost:8091";
-const API_PREFIX = process.env.VUE_APP_API_PREFIX || "/api/v1";
+const API_PREFIX = process.env.VUE_APP_API_PREFIX || "/api/v2";
 const API = API_HOST + API_PREFIX;
 
 export default {
