1edc833561b34acd64a829b90aeb8988c69f2a15
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2020-10-20 14:38:53 +0200

    [PIN-670] Preparing Vendors page

---
+++ src/views/Configuration/Vendors.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<VendorsList />
+	</div>
+</template>
+
+<script>
+import VendorsList from "@/components/Configuration/VendorsList";
+
+export default {
+	name: "Vendors",
+
+	components: {
+		VendorsList,
+	},
+};
+</script>
