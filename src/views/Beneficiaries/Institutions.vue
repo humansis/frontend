edc7e7e7598096f57a31491ba7558870ea6580d0
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2020-10-22 13:42:56 +0200

    [PIN-661] Adding page Institutions with grid

--- src/views/Beneficiaries/Institutions.vue
+++ src/views/Beneficiaries/Institutions.vue
@@ -1,70 +1,17 @@
 <template>
 	<div>
-		<h2 class="title">institutions</h2>
-		<b-button
-			class="mb-5"
-			size="is-medium"
-			type="is-danger"
-			icon-left="plus"
-		>
-			Create
-		</b-button>
-		<DataGrid :gridData="gridData" :gridFilters="gridFilters" />
+		<InstitutionsList />
 	</div>
 </template>
 
 <script>
-import { fetcher } from "@/utils/fetcher";
-import DataGrid from "@/components/Beneficiaries/DataGrid";
+import InstitutionsList from "@/components/Beneficiaries/InstitutionsList";
 
 export default {
-	name: "Institutions",
+	name: "Vendors",
 
 	components: {
-		DataGrid,
-	},
-
-	data() {
-		return {
-			fetch: {
-				error: null,
-			},
-			gridData: [],
-			gridFilters: [],
-		};
-	},
-
-	watch: {
-		$route: "fetchData",
-	},
-
-	mounted() {
-		this.fetchData();
-	},
-
-	methods: {
-		async fetchData() {
-			try {
-				this.fetch.error = null;
-				const loadingComponent = this.$buefy.loading.open();
-
-				const { data } = await fetcher({ uri: "beneficiaries/households", auth: true });
-				const filters = await fetcher({ uri: "beneficiaries/filters", auth: true });
-
-				this.gridData = data;
-				this.gridFilters = filters.data;
-
-				loadingComponent.close();
-			} catch (error) {
-				this.handleError(error);
-			}
-		},
-
-		handleError(error) {
-			console.error(error);
-			this.fetch.loading = false;
-			this.fetch.error = error.toString();
-		},
+		InstitutionsList,
 	},
 };
 </script>
