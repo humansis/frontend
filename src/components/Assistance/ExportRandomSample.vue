dad0ac5485e2c6846ac2fd428b590a97281225fe
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2020-11-16 14:39:01 +0100

    [PIN-836] Export Random Sample component

--- src/components/Assistance/ExportRandomSample.vue
+++ src/components/Assistance/ExportRandomSample.vue
@@ -1,11 +1,17 @@
 <template>
 	<div>
-		ExportRandomSample
+		<AssistanceList change-button :add-button="false" />
 	</div>
 </template>
 
 <script>
+import AssistanceList from "@/components/Assistance/AssistanceList";
+
 export default {
 	name: "ExportRandomSample",
+
+	components: {
+		AssistanceList,
+	},
 };
 </script>
