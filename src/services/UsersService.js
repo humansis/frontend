e73617d124e46bf95e9d93c0393744a9be56e01f
David Kalianko <david.kalinko@quanti.cz>
2020-11-18 15:32:36 +0100

    [PIN-874]- Fix after CR

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,58 +1,58 @@
-import { fetcher } from "@/utils/fetcher";
+// TODO uncomment methods after implementing this endpoint
 
 export default {
+	// eslint-disable-next-line no-unused-vars
 	async getListOfUsers(page, size, sort, search = null) {
+		// const fulltext = search ? `&fulltext=${search}` : "";
+		//
+		// const { data: { data, totalCount } } = await fetcher({
+		// 	uri: `users?page=${page}&size=${size}&sort=${sort + fulltext}`,
+		// });
+		// return { data, totalCount };
 		return { data: [], totalCount: 0 };
-		// eslint-disable-next-line no-unreachable
-		const fulltext = search ? `&fulltext=${search}` : "";
-
-		const { data: { data, totalCount } } = await fetcher({
-			uri: `users?page=${page}&size=${size}&sort=${sort + fulltext}`,
-		});
-		return { data, totalCount };
 	},
 
+	// eslint-disable-next-line no-unused-vars
 	async createUser(body) {
+		// const { data, status } = await fetcher({ uri: "users", method: "POST", body });
+		// return { data, status };
 		return { data: [], status: 200 };
-		// eslint-disable-next-line no-unreachable
-		const { data, status } = await fetcher({ uri: "users", method: "POST", body });
-		return { data, status };
 	},
 
+	// eslint-disable-next-line no-unused-vars
 	async getDetailOfUser(id) {
+		// const { data: { data } } = await fetcher({
+		// 	uri: `users/${id}`,
+		// });
+		// return { data };
 		return { data: {}, totalCount: 0 };
-		// eslint-disable-next-line no-unreachable
-		const { data: { data } } = await fetcher({
-			uri: `users/${id}`,
-		});
-		return { data };
 	},
 
+	// eslint-disable-next-line no-unused-vars
 	async updateUser(id, body) {
+		// const { data, status } = await fetcher({
+		// 	uri: `users/${id}`, method: "PUT", body,
+		// });
+		// return { data, status };
 		return { data: {}, status: 200 };
-		// eslint-disable-next-line no-unreachable
-		const { data, status } = await fetcher({
-			uri: `users/${id}`, method: "PUT", body,
-		});
-		return { data, status };
 	},
 
+	// eslint-disable-next-line no-unused-vars
 	async deleteUser(id) {
+		// const { data, status } = await fetcher({
+		// 	uri: `users/${id}`, method: "DELETE",
+		// });
+		// return { data, status };
 		return { data: {}, status: 204 };
-		// eslint-disable-next-line no-unreachable
-		const { data, status } = await fetcher({
-			uri: `users/${id}`, method: "DELETE",
-		});
-		return { data, status };
 	},
 
+	// eslint-disable-next-line no-unused-vars
 	async sendHistory(id) {
+		// const { data: { data } } = await fetcher({
+		// 	uri: `users/${id}/logs`,
+		// });
+		// return { data };
 		return { data: {} };
-		// eslint-disable-next-line no-unreachable
-		const { data: { data } } = await fetcher({
-			uri: `users/${id}/logs`,
-		});
-		return { data };
 	},
 
 };
