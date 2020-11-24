7c1683b32041151273a4d8cd501d0cd57fa80b4d
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2020-11-24 13:40:43 +0100

    [PIN-820] Household summary

--- src/components/Beneficiaries/Household/Summary.vue
+++ src/components/Beneficiaries/Household/Summary.vue
@@ -1,11 +1,49 @@
 <template>
 	<div>
-		<h2 class="title">Summary Tab</h2>
+		<h4 class="title is-4 has-text-centered mt-5">
+			Please Add This Household To One Or More Project
+		</h4>
+		<div class="columns mb-5">
+			<div class="column is-half is-offset-one-quarter">
+				<b-field label="Projects">
+					<MultiSelect
+						v-model="selectedProjects"
+						searchable
+						multiple
+						:options="options.projects"
+					/>
+				</b-field>
+			</div>
+		</div>
+		<h4 class="title is-4 has-text-centered">Household Information Summary</h4>
+		<div class="columns mb-5">
+			<div class="column is-half">
+				<div class="box">
+					<p class="title is-6">Current Address</p>
+					<p class="subtitle is-4">edbwwkeh</p>
+				</div>
+			</div>
+			<div class="column is-half">
+				<div class="box">
+					<p class="title is-6">Current Location</p>
+					<p class="subtitle is-4">edbwwkeh</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
 	name: "Summary",
+
+	data() {
+		return {
+			selectedProjects: [],
+			options: {
+				projects: [],
+			},
+		};
+	},
 };
 </script>
