5e776ec0b61fa01ee9708230da0beff46a6e6257
David Kalianko <david.kalinko@quanti.cz>
2021-01-12 14:49:03 +0100

    [PIN-1208]- Integrate locales

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
+import i18n from "@/plugins/i18n";
 
 import "@/plugins/buefy";
 import "@/plugins/vuelidate";
@@ -15,5 +16,6 @@ Vue.config.productionTip = false;
 new Vue({
 	router,
 	store,
+	i18n,
 	render: (h) => h(App),
 }).$mount("#app");
