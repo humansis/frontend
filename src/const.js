c4b42387d8e7e0094fb678dc85330ef2c7b4ceb2
Vojtech Svoboda <vojta@svoboda.tech>
2021-02-02 15:17:53 +0100

    Removed mock api

--- src/const.js
+++ src/const.js
@@ -1,9 +1,3 @@
-// TODO remove unnecessary host and prefix and API_ON
-const API_ON = false;
-const API_HOST_MOCK = process.env.VUE_APP_API_HOST_MOCK || "http://localhost:8091";
-const API_PREFIX_MOCK = process.env.VUE_APP_API_PREFIX_MOCK || "/api/v2";
-const API_MOCK = API_HOST_MOCK + API_PREFIX_MOCK;
-
 const API_HOST = process.env.VUE_APP_API_HOST || "http://localhost:8087";
 const API_PREFIX = process.env.VUE_APP_API_PREFIX || "/api/basic";
 const API = API_HOST + API_PREFIX;
@@ -13,12 +7,8 @@ const DEFAULT_LANGUAGE = "EN";
 
 export default {
 	API_HOST,
-	API_HOST_MOCK,
 	API_PREFIX,
-	API_PREFIX_MOCK,
 	API,
-	API_MOCK,
-	API_ON,
 	DEFAULT_COUNTRY,
 	DEFAULT_LANGUAGE,
 };
