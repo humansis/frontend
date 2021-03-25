dd081a629c6091d6fe7e5d08e32f76b19381ed9e
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2021-03-25 11:41:21 +0100

    [PIN-1500] i18n and changing languages fix

--- src/const.js
+++ src/const.js
@@ -3,7 +3,7 @@ const API_PREFIX = process.env.VUE_APP_API_PREFIX || "/api/basic";
 const API = API_HOST + API_PREFIX;
 
 const DEFAULT_COUNTRY = "KHM";
-const DEFAULT_LANGUAGE = "EN";
+const DEFAULT_LANGUAGE = "en";
 
 export default {
 	API_HOST,
