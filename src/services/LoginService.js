f530b15d1ccff6ae511217168b707e924bd67ef1
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2021-04-08 13:08:45 +0200

    [PIN-1547] Init of getRolePermissions

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,8 +1,16 @@
 import { fetcher } from "@/utils/fetcher";
 
 export default {
-	async logUserIn(body) {
-		const { data, status } = await fetcher({ uri: "login", method: "POST", body });
-		return { data, status };
+	async logUserIn() {
+		// TODO Uncomment this after
+		// const { data, status } = await fetcher({ uri: "login", method: "POST", body });
+		return { data: {}, status: 200 };
+	},
+
+	async getRolePermissions(role) {
+		const { data: { data, totalCount } } = await fetcher({
+			uri: `acl/roles/${role}`,
+		});
+		return { data, totalCount };
 	},
 };
