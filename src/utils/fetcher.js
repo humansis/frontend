64167e488ca28d0a4258086b92138af54c0fb459
David Kalianko <david.kalinko@quanti.cz>
2021-04-20 11:23:27 +0200

    [PIN-1525]- Fix vendors delete

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -28,6 +28,11 @@ export const getResponseJSON = async (response, download = false) => {
 	const unauthorized = response.status === 401;
 	const forbidden = response.status === 403;
 	const notFound = response.status === 404;
+	const noContent = response.status === 204;
+
+	if (noContent) {
+		return { data: null, status: response.status };
+	}
 
 	if (forbidden) {
 		router.push({ name: "NotFound" });
