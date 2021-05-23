38e870fe5634a3990247a09dc66added0bf5ef30
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2021-05-23 22:08:57 +0200

    [PIN-1831] Added all of API for Imports

--- src/services/ImportService.js
+++ src/services/ImportService.js
@@ -1,4 +1,4 @@
-import { fetcher, filtersToUri } from "@/utils/fetcher";
+import { fetcher, filtersToUri, upload } from "@/utils/fetcher";
 
 export default {
 	async getListOfImports(page, size, sort, search = null, filters = null) {
@@ -16,6 +16,93 @@ export default {
 
 	async createImport(body) {
 		const { data, status } = await fetcher({ uri: "imports", method: "POST", body });
+		return { data, status };
+	},
+
+	async getDetailOfImport(importId) {
+		const { data, status } = await fetcher({
+			uri: `imports/${importId}`,
+		});
+		return { data, status };
+	},
+
+	async changeImportState(importId, state) {
+		const { data, status } = await fetcher({
+			uri: `imports/${importId}`,
+			method: "PATCH",
+			body: {
+				status: state,
+			},
+		});
+		return { data, status };
+	},
+
+	async getFilesInImport(importId) {
+		const { data, status } = await fetcher({
+			uri: `imports/${importId}/files`,
+		});
+		return { data, status };
+	},
+
+	async uploadFilesIntoImport(importId, files) {
+		const formData = new FormData();
+
+		// TODO Iteration for files
+		formData.append("file", files);
+
+		const { data, status } = await upload({
+			uri: `imports/${importId}/files`,
+			method: "POST",
+			body: formData,
+		});
+		return { data, status };
+	},
+
+	async removeFilesFromImport(fileId) {
+		const { data, status } = await upload({
+			uri: `imports/files/${fileId}`,
+			method: "DELETE",
+		});
+		return { data, status };
+	},
+
+	async getDuplicitiesInImport(importId) {
+		const { data, status } = await fetcher({
+			uri: `imports/${importId}/duplicities`,
+		});
+		return { data, status };
+	},
+
+	async getStatisticsInImport(importId) {
+		const { data, status } = await fetcher({
+			uri: `imports/${importId}/statistics`,
+		});
+		return { data, status };
+	},
+
+	async getFileWithInvalidEntriesFromImport(importId) {
+		const { data, status } = await fetcher({
+			uri: `imports/${importId}/invalid-files`,
+		});
+		return { data, status };
+	},
+
+	async getImportItemDetail(queueId) {
+		const { data, status } = await fetcher({
+			uri: `imports/queue/${queueId}`,
+		});
+		return { data, status };
+	},
+
+	async resolveImportItemDuplicity(queueId, state, acceptedDuplicityId) {
+		const { data, status } = await fetcher({
+			uri: `imports/queue/${queueId}`,
+			method: "PATCH",
+			body: {
+				status: state,
+				acceptedDuplicityId,
+			},
+		});
 		return { data, status };
 	},
 };
