6fab7ea54f3a3d42bd201cb0429994b7854b981a
Jakub Dvořák <dvorak.jakub.24@gmail.com>
2021-06-13 15:55:59 +0200

    [PIN-1891] Added sectors limited by project sectors

--- src/services/SectorsService.js
+++ src/services/SectorsService.js
@@ -6,6 +6,12 @@ export default {
 		return { data, totalCount };
 	},
 
+	async getListOfProjectSectors(projectId) {
+		// TODO Endpoint needs to have a different prefix
+		const { data: { data, totalCount } } = await fetcher({ uri: `projects/${projectId}/sectors` });
+		return { data, totalCount };
+	},
+
 	async getListOfSubSectors(code) {
 		const { data: { data, totalCount } } = await fetcher({ uri: `sectors/${code}/subsectors` });
 		return { data, totalCount };
