ad469d066c3221c670db34af1e1bfd99ad4fe6d3
Tomáš Hromník <tomas.hromnik@quanti.cz>
2022-10-17 11:11:29 +0200

    [PIN-4087] Better code style for calendarHelper

--- src/mixins/calendarHelper.js
+++ src/mixins/calendarHelper.js
@@ -3,7 +3,20 @@ import i18n from "@/plugins/i18n";
 export default {
 	methods: {
 		months() {
-			return [i18n.t("January"), i18n.t("February"), i18n.t("March"), i18n.t("April"), i18n.t("May"), i18n.t("June"), i18n.t("July"), i18n.t("August"), i18n.t("September"), i18n.t("October"), i18n.t("November"), i18n.t("December")];
+			return [
+				i18n.t("January"),
+				i18n.t("February"),
+				i18n.t("March"),
+				i18n.t("April"),
+				i18n.t("May"),
+				i18n.t("June"),
+				i18n.t("July"),
+				i18n.t("August"),
+				i18n.t("September"),
+				i18n.t("October"),
+				i18n.t("November"),
+				i18n.t("December"),
+			];
 		},
 	},
 };
