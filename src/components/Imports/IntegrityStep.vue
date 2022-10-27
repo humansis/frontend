035fc415d924a53d4c2411929726a912460206a3
Dominik Pavlica <dominik.pavlica@quanti.cz>
2022-10-27 15:38:20 +0000

    [PIN-3848] Get affected records button is displayed only in case when status...

--- src/components/Imports/IntegrityStep.vue
+++ src/components/Imports/IntegrityStep.vue
@@ -184,7 +184,9 @@
 											</small>
 										</div>
 									</td>
-									<td v-if="index === 0" class="has-text-right">
+									<td v-if="index === 0 && itegrityCheckFailed"
+										class="has-text-right"
+									>
 										<b-button
 											type="is-info"
 											icon-right="file-download"
@@ -391,6 +393,10 @@ export default {
 			return this.importStatus !== consts.STATUS.FINISH
 				&& this.importStatus !== consts.STATUS.CANCEL
 				&& this.importStatus !== consts.STATUS.IMPORTING;
+		},
+
+		itegrityCheckFailed() {
+			return this.statistics.status === consts.STATUS.INTEGRITY_CHECK_FAILED;
 		},
 	},
 
