293a853cfcddcdea87dab249683cc564f326e6ac
jakubdvorak <jakub.dvorak@quanti.cz>
2022-11-30 14:28:23 +0100

    [PIN-4358] Changed behaviour of search input

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,9 +5,14 @@
 			:placeholder="$t('Search')"
 			type="search"
 			icon="search"
-			:loading="loading"
-			@keypress.enter.native="onSearch"
 		/>
+		<div class="control">
+			<b-button
+				icon-left="search"
+				class="button is-primary"
+				@click="search"
+			/>
+		</div>
 	</b-field>
 </template>
 
@@ -29,15 +34,7 @@ export default {
 	data() {
 		return {
 			value: "",
-			timer: null,
-			loading: false,
 		};
-	},
-
-	watch: {
-		value() {
-			this.onSearch();
-		},
 	},
 
 	mounted() {
@@ -47,22 +44,8 @@ export default {
 	},
 
 	methods: {
-		onSearch(event) {
-			if (this.backendSearch) {
-				clearTimeout(this.timer);
-				this.loading = true;
-				if (event) {
-					this.$emit("search", this.value);
-					this.loading = false;
-				} else {
-					this.timer = setTimeout(() => {
-						this.$emit("search", this.value);
-						this.loading = false;
-					}, 1000);
-				}
-			} else {
-				this.$emit("search", this.value);
-			}
+		search() {
+			this.$emit("search", this.value);
 		},
 	},
 };
