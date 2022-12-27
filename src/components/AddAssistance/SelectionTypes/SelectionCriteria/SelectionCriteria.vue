f8ecfa989d0e730282fbbe3a0ed52382c30b4932
Dominik Pavlica <dominik.pavlica@quanti.cz>
2022-12-27 12:10:52 +0100

    [PIN-4498] Fix sending threshold value for vulnerability-scores endpoint and add condition for update button

--- src/components/AddAssistance/SelectionTypes/SelectionCriteria/SelectionCriteria.vue
+++ src/components/AddAssistance/SelectionTypes/SelectionCriteria/SelectionCriteria.vue
@@ -113,7 +113,7 @@
 				<b-button
 					class="vulnerability-update-button is-align-self-center ml-2"
 					type="is-primary"
-					:disabled="calculationLoading || !groups.length"
+					:disabled="isUpdateButtonEnabled"
 					@click="updateVulnerabilityScores"
 				>
 					{{ $t('Update') }}
@@ -251,6 +251,16 @@ export default {
 		isExportButtonDisabled() {
 			return this.vulnerabilityScoreTouched || this.calculationLoading
 				|| !this.groups.length || !this.totalCount;
+		},
+
+		isMinVulnerabilityScoreFloat() {
+			return Number(this.minimumSelectionScore) === this.minimumSelectionScore
+				&& this.minimumSelectionScore % 1 !== 0;
+		},
+
+		isUpdateButtonEnabled() {
+			return this.calculationLoading || !this.groups.length
+				|| this.isMinVulnerabilityScoreFloat;
 		},
 	},
 
@@ -450,10 +460,10 @@ export default {
 			}
 
 			if (assistanceBody.selectionCriteria?.length) {
-				await this.calculationOfAssistanceBeneficiaries({ assistanceBody, totalCount });
-				await this.calculationOfAssistanceBeneficiariesScores(
-					{ assistanceBody, totalCount },
-				);
+				await this.calculationOfAssistanceBeneficiaries({
+					assistanceBody, totalCount });
+				await this.calculationOfAssistanceBeneficiariesScores({
+					assistanceBody });
 			} else {
 				this.totalCount = 0;
 				this.countOf = 0;
@@ -498,7 +508,7 @@ export default {
 		},
 
 		async calculationOfAssistanceBeneficiariesScores(
-			{ assistanceBody, totalCount },
+			{ assistanceBody },
 		) {
 			const beneficiaryIds = this.totalBeneficiariesData?.map(({ id }) => id) || [];
 
@@ -506,7 +516,7 @@ export default {
 				beneficiaryIds,
 				sector: assistanceBody.sector,
 				scoringBlueprintId: this.scoringType?.id || null,
-				threshold: totalCount ? null : this.minimumSelectionScore,
+				threshold: this.minimumSelectionScore,
 			};
 
 			if (beneficiaryIds.length) {
