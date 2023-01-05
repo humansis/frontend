7f0665fc7bfb05315da90a5df79803acd5a9c5d5
Dominik Pavlica <dominik.pavlica@quanti.cz>
2023-01-05 08:49:10 +0000

    [PIN-4464] Prepared new structure for const

---
+++ src/consts/index.js
@@ -0,0 +1,15 @@
+import ASSISTANCE from "./assistance.js";
+import EXPORT from "./export.js";
+import FILTER from "./filter.js";
+import IMPORT from "./import.js";
+import ROLE from "./role.js";
+import GENERAL from "./general.js";
+
+export {
+	ASSISTANCE,
+	EXPORT,
+	FILTER,
+	IMPORT,
+	ROLE,
+	GENERAL,
+};
