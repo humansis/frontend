40af709404f32c2b8df00fc931f2161022305eb4
Dominik Pavlica <dominik.pavlica@quanti.cz>
2023-02-27 13:25:13 +0100

    [PIN-4731] Change condition, when admNames in localStorage are empty

--- src/layout/NavBar.vue
+++ src/layout/NavBar.vue
@@ -127,7 +127,7 @@ export default {
 
 	async created() {
 		if (!this.icons) await this.fetchIcons();
-		if (!this.admNames) await this.fetchAdmNames();
+		if (!Object.hasOwn(this.admNames, "adm1")) await this.fetchAdmNames();
 		this.setTooltip();
 	},
 
