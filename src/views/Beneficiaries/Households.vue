90d39534a3ca9dc6be467a3d5c898d1961a68435
Dominik Pavlica <dominik.pavlica@quanti.cz>
2023-10-24 17:01:09 +0200

    [PIN-5507] Added fulltext on export households

--- src/views/Beneficiaries/Households.vue
+++ src/views/Beneficiaries/Households.vue
@@ -448,10 +448,15 @@ export default {
 					try {
 						this.exportControl.loading = true;
 
+						const filters = {
+							...this.filters,
+							...(this.table.searchPhrase && { fulltext: this.table.searchPhrase }),
+						};
+
 						const { data, status, message } = await BeneficiariesService.exportHouseholds(
 							format,
 							ids,
-							this.filters,
+							filters,
 						);
 
 						downloadFile(data, filename, status, format, message);
