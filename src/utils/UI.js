d5b2c7695e578cad35ca28db75a260494a6a7f84
Dominik Pavlica <dominik.pavlica@quanti.cz>
2024-04-02 09:51:14 +0200

    [PIN-6109] Added comment

--- src/utils/UI.js
+++ src/utils/UI.js
@@ -1,6 +1,7 @@
 import { toast } from "vuetify-sonner";
 
 export const Notification = (message, color, { indefinite = false, isCloseable = true } = {}) => {
+	// Due to bug in the vuetify-sonner library (stuck notification)
 	setTimeout(() => {
 		toast(message, {
 			duration: indefinite ? Number.POSITIVE_INFINITY : 30000,
